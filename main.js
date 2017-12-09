3e67c73f1c4468f9f390ab448e0ee208e0143fa7
JAicewizard <jaap.aarts1@gmail.com>
2017-12-09 18:56:01 +0100

    added support for images, make sure the image id isnt above the pictures value.

--- main.js
+++ main.js
@@ -22,11 +22,56 @@ var config = {
 };
 firebase.initializeApp(config);
 
+function getColorCSS(c) {
+  var ele = document.createElement("div");
+  ele.style.color = c;
+  return ele.style.color.split(/\s+/).join('').toLowerCase();
+}
 
+load = 1;
 var color = firebase.database().ref('id/' + getParameterByName("id"));
 color.on("value", function(colors) {
-  document.getElementById("img1_1").setAttribute("hidden", "hidden")
-  document.getElementById("img1_2").setAttribute("hidden", "hidden")
-  document.getElementById(colors.val().color.color_1 + "_1").removeAttribute("hidden")
-  document.getElementById(colors.val().color.color_2 + "_2").removeAttribute("hidden")
+
+  if(load == 1){
+   load = 0;
+   for(i=1;i<=colors.val().pictures;i++){
+       var image = document.createElement("img");
+       image.id = "img_"+i;
+       image.src = "assets/img_"+i+".png";
+       image.setAttribute("hidden","hidden");
+      
+       
+       document.getElementById("images").appendChild(image);
+   }
+  }
+  code = {}
+  code[0] = colors.val().color.color_1;
+  code[1] = colors.val().color.color_2;
+  for(i=1;i<=2;i++){
+    console.log(code[i-1]);
+    string = getColorCSS(code[i-1]);
+    if(string == ""){
+        if(!isNaN(code[i-1])){
+          var img = document.getElementById("img_"+code[i-1]);
+          img.removeAttribute("hidden");
+          if(document.getElementById("color_"+i).firstElementChild!=null){
+            document.getElementById("color_"+i).firstElementChild.remove();
+          }
+          document.getElementById("color_"+i).appendChild(img);
+          document.getElementById("color_"+i).firstElementChild.setAttribute("width","100%");
+        }else{
+
+        }
+    }else{
+      console.log("its a colour-code")
+      if(document.getElementById("color_"+i).firstElementChild!=null){
+        document.getElementById("color_"+i).firstElementChild.remove();
+      }
+      document.getElementById("color_"+i).style.backgroundColor = string;
+    }
+  }
+  //document.getElementById("img1_1").setAttribute("hidden", "hidden")
+  //document.getElementById("img1_2").setAttribute("hidden", "hidden")
+  //document.getElementById(colors.val().color.color_1 + "_1").removeAttribute("hidden")
+  //document.getElementById(colors.val().color.color_2 + "_2").removeAttribute("hidden")
 });
