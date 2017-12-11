3349ef5b7c429eb964be37a39a6d780e33012ff8
JAicewizard <jaap.aarts1@gmail.com>
2017-12-11 15:42:13 +0100

    go the go side working

--- go/main.go
+++ go/main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
 	"strings"
 	"time"
 )
 
+var (
+	amountImages int
+	colourList   []string
+	options      map[int]string
+	closing      bool
+)
+
 func calcColours(lastTime time.Time) {
-	newTime := time.Now()
-	diference := time.Since(lastTime)
-	if diference < time.Second {
-
+	var (
+		diference time.Duration
+		newTime   time.Time
+		random    int
+		random2   int
+	)
+	diference = time.Since(lastTime)
+	if closing {
+		log.Println("closing down")
+		os.Exit(0)
+	} else if diference > 0 {
+		time.Sleep(time.Second - diference)
 	}
-	calcColours(newTime)
-}
-
-func main() {
-	Time := time.Now()
-
-	go calcColours(Time)
+	random = rand.Intn(len(options)) + 1
+	random2 = rand.Intn(len(options)) + 1
+	log.Println(options[random])
+	log.Println(random)
+	log.Println(options[random])
+	log.Println(random)
+	log.Println("----------------------------------------------------")
 
 	body := strings.NewReader(
 		`{
-		"color_1":"1",
-		"color_2":"blue"
+		"color_1":"` + options[random] + `",
+		"color_2":"` + options[random2] + `"
 		}`)
 	req, err := http.NewRequest("PUT", "https://synchronozedtablets.firebaseio.com/id/1/color.json", body)
 	if err != nil {
@@ -36,5 +57,38 @@ func main() {
 		// handle err
 	}
 	resp.Body.Close()
+	newTime = time.Now()
+	calcColours(newTime)
+}
+
+func main() {
+	totImages, _ := strconv.ParseInt(os.Args[2], 0, 46)
+	amountImages = int(totImages)
+	options = make(map[int]string)
+	input := os.Args[1]
+	var colours []string
+	log.Println(input)
+	_ = json.Unmarshal([]byte(input), &colours)
+	log.Printf("Unmarshaled: %v", colours)
+	i := 1
+	options = make(map[int]string, amountImages+len(colours))
+
+	for ; i <= amountImages; i++ {
+		options[i] = strconv.Itoa(i)
+		println(options[i])
+	}
+	a := i - 1
+	for i := 1; i < len(colours); i++ {
+		options[a+i] = colours[i]
+	}
+	log.Println(options)
+
+	Time := time.Now()
+	go calcColours(Time)
+	var signal_channel chan os.Signal
+	signal_channel = make(chan os.Signal, 1)
+	signal.Notify(signal_channel, os.Interrupt)
+	<-signal_channel
+	closing = true
 
 }
