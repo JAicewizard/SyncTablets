c934b7b22251960301f97d3fefff789e96632ae3
JAicewizard <jaap.aarts1@gmail.com>
2017-12-11 18:00:57 +0100

    automaticly generates colours

--- go/main.go
+++ go/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -17,56 +18,18 @@ var (
 	colourList   []string
 	options      map[int]string
 	closing      bool
+	idCountTot   int
+	values       map[string]map[string]map[string]string
 )
-
-func calcColours(lastTime time.Time) {
-	var (
-		diference time.Duration
-		newTime   time.Time
-		random    int
-		random2   int
-	)
-	diference = time.Since(lastTime)
-	if closing {
-		log.Println("closing down")
-		os.Exit(0)
-	} else if diference > 0 {
-		time.Sleep(time.Second - diference)
-	}
-	random = rand.Intn(len(options)) + 1
-	random2 = rand.Intn(len(options)) + 1
-	log.Println(options[random])
-	log.Println(random)
-	log.Println(options[random2])
-	log.Println(random2)
-	log.Println("----------------------------------------------------")
-
-	body := strings.NewReader(
-		`{
-		"color_1":"` + options[random] + `",
-		"color_2":"` + options[random2] + `"
-		}`)
-	req, err := http.NewRequest("PUT", "https://synchronozedtablets.firebaseio.com/id/1/color.json", body)
-	if err != nil {
-		// handle err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		// handle err
-	}
-	resp.Body.Close()
-	newTime = time.Now()
-	calcColours(newTime)
-}
 
 func main() {
 	var (
 		colours []string
 	)
-	options = make(map[int]string, amountImages+len(colours)+1)
+	Time := time.Now()
+	go getCurrentIDs(Time)
 
+	options = make(map[int]string, amountImages+len(colours)+1)
 	totImages, _ := strconv.ParseInt(os.Args[2], 0, 46)
 	amountImages = int(totImages)
 
@@ -78,7 +41,6 @@ func main() {
 
 	for ; i <= amountImages; i++ {
 		options[i] = strconv.Itoa(i)
-		println(options[i])
 	}
 	a := i - 1
 	for i := 0; i < len(colours); i++ {
@@ -86,12 +48,94 @@ func main() {
 	}
 	log.Println(options)
 
-	Time := time.Now()
 	go calcColours(Time)
+	go getCurrentIDs(Time)
 	var signal_channel chan os.Signal
 	signal_channel = make(chan os.Signal, 1)
 	signal.Notify(signal_channel, os.Interrupt)
 	<-signal_channel
 	closing = true
 
+}
+
+func calcColours(lastTime time.Time) {
+	var (
+		diference   time.Duration
+		newTime     time.Time
+		random      string
+		random2     string
+		idValues    map[string]map[string]string
+		colorValues map[string]string
+	)
+
+	newTime = time.Now()
+	diference = time.Since(lastTime)
+	if closing {
+		log.Println("closing down")
+		os.Exit(0)
+	} else if diference > 0 {
+		time.Sleep(time.Second*3 - diference)
+	}
+	values = make(map[string]map[string]map[string]string, idCountTot)
+	random = options[rand.Intn(len(options))+1]
+	for i := 1; i < idCountTot; i++ {
+		idValues = make(map[string]map[string]string)
+		colorValues = make(map[string]string)
+		random2 = options[rand.Intn(len(options))+1]
+		colorValues["color_1"] = random
+		colorValues["color_2"] = random2
+		idValues["color"] = colorValues
+		values[strconv.Itoa(i)] = idValues
+		random = random2
+	}
+	bits, _ := json.Marshal(values)
+	body := bytes.NewReader(bits)
+	log.Println(string(bits))
+	req, err := http.NewRequest("PUT", "https://synchronozedtablets.firebaseio.com/id.json", body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// handle err
+	}
+	resp.Body.Close()
+	calcColours(newTime)
+}
+
+func getCurrentIDs(lastTime time.Time) {
+
+	var (
+		diference time.Duration
+		newTime   time.Time
+		idCount   []byte
+	)
+
+	log.Println("im here now")
+
+	resp, err := http.Get("https://synchronozedtablets.firebaseio.com/idCount.json?")
+	if err != nil {
+		log.Println("could not get the date required for the setup, could not find idCount")
+		os.Exit(0)
+	}
+	log.Println(resp)
+	defer resp.Body.Close()
+	idCount, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	idCountTot, _ = strconv.Atoi(string(idCount))
+	idCountTot = idCountTot + 10
+	newTime = time.Now()
+	diference = time.Since(lastTime)
+	if closing {
+		log.Println("closing down")
+		os.Exit(0)
+	} else if diference > 0 {
+		time.Sleep(time.Minute - diference)
+	}
+	getCurrentIDs(newTime)
 }
